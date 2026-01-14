ef59c85d44f3eb843a320d29e86570c916f586f1
souvik-maity-2002 <iamsouvikmaity@2002gmail.com>
2026-01-14 12:45:44 +0530

    remove env file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://codereview-6453.onrender.com', { code })
     setReview(response.data)
   }
 
